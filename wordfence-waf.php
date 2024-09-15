9815f1e68c8ad7b3ad014b6a0ee04bba61f48fc6
JordanoP11 <vjponce@tes.edu.ec>
2024-09-15 01:45:58 -0500

    ToCupBoard
    Página web E-commerce.

---
+++ wordfence-waf.php
@@ -0,0 +1,11 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+// This file was the current value of auto_prepend_file during the Wordfence WAF installation (Sat, 14 Sep 2024 23:18:33 +0000)
+if (file_exists('/var/www/errors/override.php')) {
+	include_once '/var/www/errors/override.php';
+}
+if (file_exists(__DIR__.'/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", __DIR__.'/wp-content/wflogs/');
+	include_once __DIR__.'/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
